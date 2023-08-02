0acfdfbbee8838d8cf92685aca98e89a69eb25e6
Esh527 <edesrihari33@gmail.com>
2023-08-02 14:58:04 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+import HeaderComponent from './components/Header_Component';
+import TimelineComponent from './components/Timeline_Component';
+import NavigationComponent from './components/Navigation_Component';
+import TrailClosuresComponent from './components/TrailClosures_Component';
+import BookingSectionComponent from './components/Booking_Section_Component';
+import FooterComponent from './components/Footer_Component';
+import PopupComponent from './components/Popup_Component';
+import FormPageComponent from './components/FormPage_Component';
+import './styles.css';
 
 function App() {
+  const [popupIsOpen, setPopupIsOpen] = useState(false);
+
+  const openPopup = () => {
+    setPopupIsOpen(true);
+  };
+
+  const closePopup = () => {
+    setPopupIsOpen(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <HeaderComponent />
+      {/* <NavigationComponent/> */}
+      <TimelineComponent />
+      <TrailClosuresComponent/>
+      <BookingSectionComponent/>
+      <br/>
+      <FooterComponent/>
+      <div className="popup-button">
+        <button onClick={openPopup}>Learn more about November in Maasai Mara</button>
+      </div>
+      <PopupComponent isOpen={popupIsOpen} onClose={closePopup} />
+
+      <br/><br/>
+
+      <br/><br/>
+      <FormPageComponent/>
     </div>
   );
 }
